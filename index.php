df3a0718c018d072a4ab0121711d0f0dee7a53b2
Ken Tsang <ken@xilva.com>
2015-05-28 20:30:43 +0800

    installed and setup idiorm

--- index.php
+++ index.php
@@ -29,6 +29,20 @@ else {
 
 
 
+/** 
+ * Setup Idiorm (ORM)
+ */
+ORM::configure('mysql:host='.DB_HOST.';dbname='.DB_NAME);
+ORM::configure('username', DB_USER);
+ORM::configure('password', DB_PASSWORD);
+ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+ORM::configure('logging', DB_LOG);
+
+
+
+
+
+
 $app = new \Slim\Slim();
 
 $app->get('/', function () use ($app) {
